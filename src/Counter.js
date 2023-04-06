35a0c35be20a9b948632dbbf52ddf5ae92908063
mhmt-trk <121402089+mhmt-trk@users.noreply.github.com>
2023-04-06 18:10:43 +0300

    ilk react

---
+++ src/Counter.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+export class Counter extends Component {
+  state = { number: 0 };
+  increment = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+  decrement = () => {
+    this.setState({ number: this.state.number - 1 });
+  };
+
+  render() {
+    const buttonStyle = {
+      margin: "5px",
+      cursor: "pointer",
+      padding: "5px 30px",
+      borderRadius: "10px",
+    };
+
+    return (
+      <div>
+        <h1>Counter</h1>
+        <h2>{this.state.number}</h2>
+        <button onClick={() => this.decrement()} style={buttonStyle}>
+          -
+        </button>
+        <button onClick={this.increment} style={buttonStyle}>
+          +
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
